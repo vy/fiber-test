037cb3cb239e71ca7f3ae2c34c18a65d4e09b830
Volkan Yazici <volkan.yazici@gmail.com>
2018-12-15 22:10:04 +0100

    Decrease default ring.messagePassingCount property to 1e6, tests are taking ages.

--- common/src/main/java/com/vlkan/fibertest/ring/RingBenchmarkConfig.java
+++ common/src/main/java/com/vlkan/fibertest/ring/RingBenchmarkConfig.java
@@ -8,6 +8,6 @@ public enum RingBenchmarkConfig {;
 
     public static final int WORKER_COUNT = PropertyHelper.readPositiveNonZeroIntegerProperty("ring.workerCount", "50");
 
-    public static final int MESSAGE_PASSING_COUNT = PropertyHelper.readPositiveNonZeroIntegerProperty("ring.messagePassingCount", "10000000");
+    public static final int MESSAGE_PASSING_COUNT = PropertyHelper.readPositiveNonZeroIntegerProperty("ring.messagePassingCount", "1000000");
 
 }
